f514d2ebbeab533a880f9e616784f2f4196be4f1
hamidreza <dolati.hamidreza78@gmail.com>
2026-06-20 14:58:42 +0330

    first step done. it can read a file and find line that have a specific simple word

--- grep_clone/main.c
+++ grep_clone/main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
-int main() {
-    // todo:  open file(s)
-    // todo:  read files line by line
-    // todo:  search for pattern in each line
-    // todo:  if found print the line
+#include <string.h>
 
-    printf("Hello, World!\n");
+int main(int argc, char** argv) {
+    if (argc < 3) fprintf(stderr, "Usage: %s <word> <filename>\n", argv[0]);
+
+    // todo:  open file(s)
+    FILE* file = fopen(argv[2], "r");
+    if (file == NULL) fprintf(stderr, "Error: Cannot open file %s\n", argv[2]);
+
+    // todo:  read files line by line
+    char line[1024] = {0};
+    while (fgets(line, sizeof(line), file) != NULL) {
+        // todo:  search for pattern in each line
+        // todo:  if found print the line
+        if (strstr(line, argv[1])) printf("%s", line);
+    }
 }
