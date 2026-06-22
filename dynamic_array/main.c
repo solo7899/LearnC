fd1b7d1a03e557ba1250b1ac5e4c1dc25a6632a9
hamidreza <dolati.hamidreza78@gmail.com>
2026-06-22 10:10:40 +0330

    creation functionality added

--- dynamic_array/main.c
+++ dynamic_array/main.c
@@ -6,7 +6,20 @@ typedef struct {
     size_t capacity;
 } darray;
 
+darray* darray_create(size_t capacity);
+void darray_destroy(darray* arr);
+void darray_push(darray* arr, int value);
+
 int main() {
     printf("dynamic array\n");
     return 0;
+}
+
+darray* darray_create(size_t capacity) {
+    darray* arr = malloc(sizeof(darray));
+    if (!arr) return NULL;
+    arr->data = malloc(capacity * sizeof(int));
+    arr->size = 0;
+    arr->capacity = capacity;
+    return arr;
 }
