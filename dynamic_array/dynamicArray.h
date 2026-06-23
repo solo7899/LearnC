40201f4aa10dcc21c6b3efc0718e7b1ede66b491
hamidreza <dolati.hamidreza78@gmail.com>
2026-06-23 09:35:38 +0330

    seperating as a lib

--- dynamic_array/dynamicArray.h
+++ dynamic_array/dynamicArray.h
@@ -1,8 +1,14 @@
 #include <stdlib.h>
 
+#ifndef D_ARRAY
+
+#define D_ARRAY 1
+
 typedef struct darray darray;
 darray* darray_create(size_t capacity);
 void darray_destroy(darray* arr);
 void darray_push(darray* arr, int value);
 int darray_pop(darray* arr, int* out);
 void darray_print(darray* arr);
+
+#endif
