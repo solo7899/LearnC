558da340dffdf4bb0a9c00e0616f403edaa60c03
hamidreza <dolati.hamidreza78@gmail.com>
2026-06-24 07:57:46 +0330

    creation function added

--- hashtable/main.c
+++ hashtable/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node node;
+
+node* hashtable_create();
 
 int main() {
     printf("hashtables...\n");
     return 0;
+}
+
+typedef struct node {
+    char* key;
+    int value;
+    node* next;
+};
+
+// implementing a simple hash table using list
+node* hashtable_create() {
+    node* hashtable = malloc(sizeof(sizeof(node) * 30));
+    if (!hashtable) return NULL;
+    return hashtable;
 }
