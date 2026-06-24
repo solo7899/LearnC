36826a70b34ff8ee3a4cbac7db0db68048f0bd6a
hamidreza <dolati.hamidreza78@gmail.com>
2026-06-24 08:45:57 +0330

    after a bunch of rewriting, put function Done..... wait till testing phase :)/(

--- hashtable/main.c
+++ hashtable/main.c
@@ -4,8 +4,9 @@
 
 typedef struct node node;
 
-node* hashtable_create();
+node** hashtable_create();
 int hashtable_hash(char* key);
+void hashtable_put(node** hashtable, char* key, int value);
 
 int main() {
     printf("hashtables...\n");
@@ -19,18 +20,40 @@ typedef struct node {
 };
 
 // implementing a simple hash table using list
-node* hashtable_create() {
-    node* hashtable = malloc(sizeof(sizeof(node) * 30));
+node** hashtable_create() {
+    node** hashtable = (node**)malloc(sizeof(sizeof(node*) * 30));
     if (!hashtable) return NULL;
+    *hashtable = 0;
     return hashtable;
 }
 
 // hashing functionality to get the index
-int hashtable_hash(char* key) {
+// making it private
+static int hashtable_hash(char* key) {
     int hash = 0;
     for (int i = 0; key[i] != '\0'; i++) {
         hash = (hash * 31) + key[i];
         hash %= 30;
     }
     return hash;
+}
+
+void hashtable_put(node** hashtable, char* key, int value) {
+    int index = hashtable_hash(key);
+    node* current = hashtable + index;
+
+    while (current->value && current->next) {
+        if (strcmp(current->key, key) == 0) {
+            current->value = value;
+            return;
+        }
+        current = current->next;
+    }
+
+    node* newNode = (node*)malloc(sizeof(node));
+    if (!current->next) return;
+    newNode->key = key;
+    newNode->value = value;
+    newNode->next = NULL;
+    current->next = newNode;
 }
