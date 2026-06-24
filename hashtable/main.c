e4ce06ef4c46a4e4a39f1a5f943449643c1ae4df
hamidreza <dolati.hamidreza78@gmail.com>
2026-06-24 08:05:25 +0330

    hashing function added

--- hashtable/main.c
+++ hashtable/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct node node;
 
 node* hashtable_create();
+int hashtable_hash(char* key);
 
 int main() {
     printf("hashtables...\n");
@@ -21,4 +23,14 @@ node* hashtable_create() {
     node* hashtable = malloc(sizeof(sizeof(node) * 30));
     if (!hashtable) return NULL;
     return hashtable;
+}
+
+// hashing functionality to get the index
+int hashtable_hash(char* key) {
+    int hash = 0;
+    for (int i = 0; key[i] != '\0'; i++) {
+        hash = (hash * 31) + key[i];
+        hash %= 30;
+    }
+    return hash;
 }
