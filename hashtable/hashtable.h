e07723e24ba41316e5f12e2b42c3e86381dc9aa2
hamidreza <dolati.hamidreza78@gmail.com>
2026-06-25 07:50:53 +0330

    seperating declartions and definitions into seperate .h and .c files

---
+++ hashtable/hashtable.h
@@ -0,0 +1,13 @@
+#ifndef HASHTABLE_H
+
+#define HASHTABLE_H 1
+
+typedef struct node node;
+
+node** hashtable_create();
+int hashtable_hash(char* key);
+void hashtable_put(node** hashtable, char* key, int value);
+static node* hashtable_newNode(char* key, int value);
+node* hashtable_read(node** hashtable, char* key);
+
+#endif
--- hashtable/main.c
+++ hashtable/main.c
@@ -1,81 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-typedef struct node node;
-
-node** hashtable_create();
-int hashtable_hash(char* key);
-void hashtable_put(node** hashtable, char* key, int value);
-static node* hashtable_newNode(char* key, int value);
-node* hashtable_read(node** hashtable, char* key);
+#include "hashtable.h"
 
 int main() {
     printf("hashtables...\n");
     return 0;
-}
-
-typedef struct node {
-    char* key;
-    int value;
-    node* next;
-};
-
-// implementing a simple hash table using list
-node** hashtable_create() {
-    node** hashtable = (node**)calloc(30, sizeof(node*));
-    if (!hashtable) return NULL;
-    return hashtable;
-}
-
-// hashing functionality to get the index
-// making it private
-static int hashtable_hash(char* key) {
-    int hash = 0;
-    for (int i = 0; key[i] != '\0'; i++) {
-        hash = (hash * 31) + key[i];
-        hash %= 30;
-    }
-    return hash;
-}
-
-void hashtable_put(node** hashtable, char* key, int value) {
-    int index = hashtable_hash(key);
-    node* current = hashtable + index;
-
-    if (!current) {
-        current = hashtable_newNode(key, value);
-        hashtable[index] = current;
-        return;
-    }
-
-    do {
-        if (strcmp(current->key, key) == 0) {
-            current->value = value;
-            return;
-        }
-        current = current->next;
-    } while (current->next);
-
-    current->next = hashtable_newNode(key, value);
-}
-
-static node* hashtable_newNode(char* key, int value) {
-    node* newNode = (node*)malloc(sizeof(node));
-    if (!newNode) return;
-    newNode->key = key;
-    newNode->value = value;
-    newNode->next = NULL;
-
-    return newNode;
-}
-
-node* hashtable_read(node** hashtable, char* key) {
-    int index = hashtable_hash(key);
-    node* current = hashtable[index];
-    while (current) {
-        if (strcmp(current->key, key) == 0) return current;
-        current = current->next;
-    }
-    return NULL;
 }
