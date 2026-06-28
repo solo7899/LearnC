c50f320e7384f6ce2dadad3a3ec3d338a1821562
solo7899 <dolati.hamidreza78@gmail.com>
2026-06-28 09:55:06 +0330

    tcp socket done,  this is gonna be tough, still doing research ...

--- port scanner/main.c
+++ port scanner/main.c
@@ -1,6 +1,27 @@
+#include <arpa/inet.h>
 #include <stdio.h>
+#include <sys/socket.h>
+
+typedef enum _conType { TCP, UDP } conType;
 
 int main() {
     printf("starting the project after some research\n");
     return 0;
+}
+
+int sock_create(conType type) {
+    int sockfd;
+
+    switch (type) {
+        case TCP:
+            sockfd = socket(AF_INET, SOCK_STREAM, 0);
+            break;
+    }
+
+    if (sockfd < 0) {
+        fprintf(stderr, "Error creating socket\n");
+        return -1;
+    }
+
+    return sockfd;
 }
