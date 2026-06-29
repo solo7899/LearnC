d3b5bdd8379a85a9866e5a9a1db1080d31cde5d9
solo7899 <dolati.hamidreza78@gmail.com>
2026-06-29 10:36:24 +0330

    tcp scanning done

--- port scanner/main.c
+++ port scanner/main.c
@@ -5,8 +5,29 @@
 
 typedef enum _conType { TCP, UDP } conType;
 
-int main() {
-    printf("starting the project after some research\n");
+int sock_create(conType type);
+int check_port(int sockfd, struct sockaddr_in* target, int port, char* buffer);
+
+int main(int argc, char** argv) {
+    // let's do just a simple tcp scanning for now and move on
+    struct sockaddr_in target;
+    char buffer[1024] = {0};
+
+    int sockfd = sock_create(TCP);
+    if (sockfd < 0) {
+        perror("socket creation failed");
+        return 1;
+    }
+
+    target.sin_family = AF_INET;
+    inet_pton(AF_INET, "142.251.211.110", &target.sin_addr.s_addr);
+
+    if (check_port(sockfd, &target, 80, buffer) < 0) {
+        fprintf(stderr, "Port 80 is closed\n");
+    } else {
+        printf("Port 80 is open\n");
+    }
+
     return 0;
 }
 
@@ -28,4 +49,21 @@ int sock_create(conType type) {
     }
 
     return sockfd;
+}
+
+int check_port(int sockfd, struct sockaddr_in* target, int port, char* buffer) {
+    target->sin_port = htons(port);
+    char* message = "\x01";
+
+    if (connect(sockfd, (struct sockaddr*)target, sizeof(*target)) < 0) {
+        perror("tcp handshake failed [connect]\n");
+        return -1;
+    }
+
+    if (send(sockfd, message, sizeof(message), 0) < 0) {
+        perror("send failed");
+        return -1;
+    }
+
+    return recv(sockfd, buffer, sizeof(buffer), 0);
 }
