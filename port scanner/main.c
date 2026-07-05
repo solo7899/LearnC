02bce94d1b19e686fba6a7cde1f72cb9fd68efbd
solo7899 <dolati.hamidreza78@gmail.com>
2026-07-05 19:51:56 +0330

    it's fucked, but less fucked than it's gonna be tommorow :_)

--- port scanner/main.c
+++ port scanner/main.c
@@ -1,10 +1,17 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
+#define MAX_THREDS 10
+
+char buffer[1024] = {0};
+int counter;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 typedef enum _conType { TCP, UDP } conType;
 
@@ -23,6 +30,12 @@ int tcp_scan(port port, struct sockaddr_in* target, char* buffer);
 void tcp_conditional_wrapper(int sockfd, struct sockaddr_in* target, int port,
                              char* buffer);
 
+typedef struct workerStruct {
+    struct sockaddr_in* target;
+    port port;
+} ws;
+int worker(void* ws);
+
 int main(int argc, char** argv) {
     if (argc < 3) {
         perror("Usage: ./port_scanner <ip_address> <port>");
@@ -33,11 +46,11 @@ int main(int argc, char** argv) {
     port port;
 
     // let's do just a simple tcp scanning for now and move on
-    struct sockaddr_in target;
-    char buffer[1024] = {0};
+    struct sockaddr_in* target;
+    target = malloc(sizeof(struct sockaddr_in));
 
-    target.sin_family = AF_INET;
-    inet_pton(AF_INET, ipaddr, &target.sin_addr.s_addr);
+    target->sin_family = AF_INET;
+    inet_pton(AF_INET, ipaddr, &target->sin_addr.s_addr);
 
     if (strchr(argv[2], '-')) {
         if (sscanf(argv[2], "%d-%d", &port.value.portRange[0],
@@ -51,7 +64,9 @@ int main(int argc, char** argv) {
         port.type = TYPE_INT;
     }
 
-    tcp_scan(port, &target, buffer);
+    tcp_scan(port, target, buffer);
+
+    free(target);
 
     return 0;
 }
@@ -104,17 +119,18 @@ int tcp_scan(port port, struct sockaddr_in* target, char* buffer) {
             break;
         case TYPE_RANGE:
 
-            for (int i = port.value.portRange[0]; i <= port.value.portRange[1];
-                 i++) {
-                int sockfd = sock_create(TCP);
-                if (sockfd < 0) {
-                    perror("socket creation failed");
-                    return -1;
-                }
+            counter = port.value.portRange[0];
+            pthread_t threads[MAX_THREDS];
+            ws* workerS;
+            workerS->target = target;
+            workerS->port = port;
 
-                tcp_conditional_wrapper(sockfd, target, i, buffer);
-                close(sockfd);
-            }
+            for (int i = 0; i < MAX_THREDS; i++)
+                pthread_create(&threads[i], NULL, (void*)worker,
+                               (void*)workerS);
+            for (int i = 0; i < MAX_THREDS; i++) pthread_join(threads[i], NULL);
+
+            pthread_mutex_destroy(&lock);
 
             break;
     }
@@ -126,4 +142,27 @@ void tcp_conditional_wrapper(int sockfd, struct sockaddr_in* target, int port,
         fprintf(stderr, "Port %d is closed\n", port);
     else
         printf("Port %d is open\n", port);
+}
+
+int worker(void* workerStruct) {
+    printf("working %d", counter);
+    ws* workS = (ws*)workerStruct;
+    while (1) {
+        if (counter > workS->port.value.portRange[1]) {
+            break;
+        }
+        int sockfd = sock_create(TCP);
+        if (sockfd < 0) {
+            perror("socket creation failed");
+            return -1;
+        }
+
+        tcp_conditional_wrapper(sockfd, (struct sockaddr_in*)workS->target,
+                                counter, buffer);
+        close(sockfd);
+
+        pthread_mutex_lock(&lock);
+        counter++;
+        pthread_mutex_unlock(&lock);
+    }
 }
