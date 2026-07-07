cac655f6a19dc11860f0c0949440960b9d66095f
solo7899 <dolati.hamidreza78@gmail.com>
2026-07-07 14:07:57 +0330

    one step closer to heaven

--- port scanner/multi_thread/main.c
+++ port scanner/multi_thread/main.c
@@ -1,19 +1,24 @@
+#include <netinet/in.h>
 #include <pthread.h>
 #include <stdio.h>
+#include <sys/socket.h>
 #include <unistd.h>
 
 #define MAX_THREAD 10
 pthread_mutex_t lock;
+char* IP;
 int counter;
 
 void tcp_ping(int port) {
     printf("Pinging port: %d\n", port);
     // Simulate some work with sleep
+
     sleep(1);
 }
 
-void* worker() {
-    while (counter < 100) {
+void* worker(void* end) {
+    int finish = *(int*)end;
+    while (counter <= finish) {
         int count;
         pthread_mutex_lock(&lock);
         count = counter++;
@@ -24,14 +29,22 @@ void* worker() {
     return NULL;
 }
 
-int main() {
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        printf("Usage: %s <ip addr> <ip range>\n", argv[0]);
+        return 1;
+    }
+
+    int start, finish;
+    sscanf(argv[2], "%d-%d", &start, &finish);
+
     pthread_mutex_init(&lock, NULL);
-    counter = 50;
+    counter = start;
 
     pthread_t threads[MAX_THREAD];
 
     for (int i = 0; i < MAX_THREAD; i++) {
-        pthread_create(&threads[i], NULL, worker, NULL);
+        pthread_create(&threads[i], NULL, worker, (void*)&finish);
     }
 
     for (int i = 0; i < MAX_THREAD; i++) {
