d4f0765d0eee26fd71a3df4c48999619e0c697ff
solo7899 <dolati.hamidreza78@gmail.com>
2026-07-16 18:02:30 +0330

    simple shell (no tokenization)

--- simple_shell/main.c
+++ simple_shell/main.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 int main() {
     while (1) {
         printf("$ ");
         char* input;
         scanf("%s", input);
-        printf("%s\n", input);
+        if (fork() == 0) {
+            execlp(input, NULL);
+        } else {
+            wait(NULL);
+        }
     }
     return 0;
 }
