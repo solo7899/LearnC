64d5215541d37aa1790d0cf3cd668d37e02a6104
solo7899 <dolati.hamidreza78@gmail.com>
2026-07-16 21:19:55 +0330

    now we can call it a simple shell

--- simple_shell/main.c
+++ simple_shell/main.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 int main() {
     while (1) {
         printf("$ ");
-        char* input;
-        scanf("%s", input);
-        if (fork() == 0) {
-            execlp(input, NULL);
+        char input[100] = {0};
+        fgets(input, 100, stdin);
+        if (strcmp(input, "exit\n") == 0) break;
+        int pid = fork();
+        if (pid == 0) {
+            char* args[10] = {0};
+            int j = 0;
+            args[j] = input;
+            int i;
+            for (i = 0; input[i] != '\n'; i++) {
+                if (input[i] == ' ') {
+                    input[i] = '\0';
+                    args[++j] = &input[i + 1];
+                }
+            }
+            input[i] = '\0';
+            args[++j] = NULL;
+            execvp(args[0], args);
         } else {
             wait(NULL);
         }
